5eacc5da4f9558dac80c13554b89849eab0368d9
seungdols <seungdols0822@gmail.com>
2017-06-05 11:58:15 +0900

    SystemRules Library version 1.5.0 -> 1.16.0

--- Chapter_02/stereo-mixedconfig/src/test/java/soundsystem/ImportJavaConfigTest.java
+++ Chapter_02/stereo-mixedconfig/src/test/java/soundsystem/ImportJavaConfigTest.java
@@ -1,10 +1,10 @@
 package soundsystem;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
